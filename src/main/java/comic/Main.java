2e5daaff584ee74a616a5fedc5eabd850576c995
Almeida Junior, Paulo <paulo.almeida.junior@accenture.com>
2020-10-17 22:50:06 -0300

    POM for Docker

--- src/main/java/comic/Main.java
+++ src/main/java/comic/Main.java
@@ -4,8 +4,6 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class Main {
-	
-	
 	public static void main(String[] args) {
 		port(8080); // Spark will run on port 8080
 		get("/retrieve", (req, res) -> {
